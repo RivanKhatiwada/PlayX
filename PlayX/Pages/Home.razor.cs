d50b9a40b539f5e509ee2b260568b594756a6b5b
RivanKhatiwada <rivankhatiwada7@gmail.com>
2026-09-08 12:25:30 +0545

    Cleaned code and made it ready for WEBRTC implementation

--- PlayX/Pages/Home.razor.cs
+++
@@ -1,8 +0,0 @@
-namespace PlayX.Pages;
-
-public partial class Home
-{
-    private void GameSelection() => Navigation.NavigateTo("/selection");
-    private void OpenAvatarCustomizer() => Navigation.NavigateTo("/customize-avatar");
-    private void OpenOptions() => Navigation.NavigateTo("/options");
-}
--- PlayX/Program.cs
+++ PlayX/Program.cs
@@ -1,21 +1,13 @@
-// Program.cs
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 using PlayX;
-using PlayX.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-
-// Register base HttpClient pointing to app origin
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// Register Application Services
-builder.Services.AddScoped<LocalStateService>();
-builder.Services.AddScoped<MultiplayerService>();
 
 await builder.Build().RunAsync();
